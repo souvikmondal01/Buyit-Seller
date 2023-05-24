799d5eb839fbf6808ecbc294fb5a346c4e11fa80
Souvik Mondal <souvikmondal2120@gmail.com>
2023-05-24 12:48:38 +0530

    App logo changed

--- app/src/main/java/com/buyit/buyitseller/activities/ProfileActivity.kt
+++ app/src/main/java/com/buyit/buyitseller/activities/ProfileActivity.kt
@@ -3,26 +3,17 @@ package com.buyit.buyitseller.activities
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.drawable.Drawable
 import android.location.Geocoder
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
 import com.buyit.buyitseller.databinding.ActivityProfileBinding
 import com.google.android.gms.location.LocationServices
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.ktx.storage
-import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Locale
 
 class ProfileActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
